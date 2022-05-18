219a93494fe5161579fb71a3734ea7817c857f34
Hannah Malm <hannahemalm616@gmail.com>
2022-05-18 16:47:51 -0500

    commits

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+class ArticlesController < ApplicationController
+  def index
+    #this will render whatever is in the views/articles/index.html.erb
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+#GET /articles requests are mapped to the index ACTION of the Articles CONTROLLER
+get "/articles", to: "articles#index"
+
+
 end
