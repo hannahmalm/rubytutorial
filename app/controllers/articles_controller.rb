b30a4642cb2e8f47c789be711f725659ee765302
Hannah Malm <hannahemalm616@gmail.com>
2022-05-18 17:05:15 -0500

    adding iterations and erb tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
     #this will render whatever is in the views/articles/index.html.erb
+    #give the view permission to view the index of articles.
+    #Typically when using Index, this is giving a full list (all)
+    #use the @to make it an INSTANCE variable and giving it access to all methods in the class
+    @articles = Article.all 
   end
 end
