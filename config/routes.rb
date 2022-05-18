5d3a913f76c498eed99d5b62458c186af0f986a6
Hannah Malm <hannahemalm616@gmail.com>
2022-05-18 16:54:58 -0500

    added in migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+#When starting the Rails server (rails s), you can set this to be the main page
+root "articles#index"
 #GET /articles requests are mapped to the index ACTION of the Articles CONTROLLER
 get "/articles", to: "articles#index"
 
